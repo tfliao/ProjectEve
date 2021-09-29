5d9bb323d7b90b70a3276790d7fde88c8010f6c8
tfliao <tingfuliao@gmail.com>
2021-09-30 00:04:23 +0800

    implement all basic functions

--- eve/cliparser.py
+++ eve/cliparser.py
@@ -13,14 +13,19 @@ KEY_LIST = 'list'
 class CliParser:
     class BadTokenFormatException(Exception):
         def __init__(self, message):
-            super(message)
+            super().__init__(message)
 
-    class CmdTreeException(Exception):
+    class CmdTreeBuildException(Exception):
         def __init__(self, message):
-            super(message)
+            super().__init__(message)
+
+    class CmdTreeParseException(Exception):
+        def __init__(self, message):
+            super().__init__(message)
 
     class CmdToken:
-        RE_VAR_PATTERN = r'@\s*(\w+)\s*(\(\w+\))?\s*(...)?'
+        RE_VAR_PATTERN = r'@\s*(\w+)\s*(\((\w+)\))?\s*(...)?'
+        ACCEPT_TYPE = ['bool', 'str', 'int']
 
         def __init__(self, token, desc = "", args = {}, hidden = True, func = None):
             self.desc = desc
@@ -44,8 +49,11 @@ class CliParser:
                 raise CliParser.BadTokenFormatException(f'token {token} is illegal')
 
             target = m.group(1)
-            type = 'str' if m.group(2) is None else m.group(2)
-            listable = m.group(3) is not None
+            type = ('str' if m.group(3) is None else m.group(3)).lower()
+            if type not in __class__.ACCEPT_TYPE:
+                raise CliParser.BadTokenFormatException(f'type {type} is not supported')
+
+            listable = m.group(4) is not None
             self.props = {
                 KEY_TARGET: target,
                 KEY_TYPE: type,
@@ -71,11 +79,13 @@ class CliParser:
         print(f'Given cmdline: {cmdline}')
         print(f'Matched cmdline: {cmdline_matched}')
         print(f'possible candidates:')
-        for ctoken, cnode in cmdtoekn.const_children.items():
-            print(f'\t{cnode.token}\t{cnode.desc}')
+        for _, cnode in cmdtoekn.const_children.items():
+            if not cnode.hidden:
+                print(f'\t{cnode.token}\t{cnode.desc}')
         if cmdtoekn.var_child is not None:
             child = cmdtoekn.var_child
-            print(f'\t{child.token}\t{child.desc}')
+            if not cnode.hidden:
+                print(f'\t{child.token}\t{child.desc}')
         """
             cmdline_matched
             possible candidates:
@@ -101,7 +111,7 @@ class CliParser:
             else:
                 if node.var_child is not None:
                     if node.var_child.token != cmd_token.token:
-                        raise CliParser.CmdTreeException(f"Conflicted variable token given [{node.var_child.token}] vs [{cmd_token.token}]")
+                        raise CliParser.CmdTreeBuildException(f"Conflicted variable token given [{node.var_child.token}] vs [{cmd_token.token}]")
                     node = node.var_child
                 else:
                     node.var_child = cmd_token
@@ -133,9 +143,24 @@ class CliParser:
         if type == 'int':
             base = 16 if token.startswith('0x') else 10
             return int(token, base)
-        raise Exception("Unknown type")
+        if type == 'bool':
+            return token.lower() in ['true', '1', 'yes', 'y', 't']
+        raise CliParser.CmdTreeParseException(f'Unknown type [{type}]')
+
+    def __capture_arg(self, node, token, args):
+        target = node.props[KEY_TARGET]
+        value = self.__cast_arg(token, node.props[KEY_TYPE])
+        if node.props[KEY_LIST]:
+            value = args.get(target, []) + [value]
+        args[target] = value
+        return node
 
     def __find_child(self, node, token, args):
+        # list type will capture all following args
+        if node.type == TOKEN_TYPE_VAR and node.props[KEY_LIST]:
+            return self.__capture_arg(node, token, args)
+
+        # check const children first
         prefix_children = []
         for ctoken, cnode in node.const_children.items():
             if ctoken == token:
@@ -143,17 +168,15 @@ class CliParser:
             if ctoken.startswith(token):
                 prefix_children.append(cnode)
 
+        # one const child solely matched the prefix
+        if len(prefix_children) == 1:
+            return prefix_children[0]
+
+        # check variable child
         if node.var_child is None:
             return None
 
-        cnode = node.var_child
-        target = cnode.props[KEY_TARGET]
-        value = self.__cast_arg(token, cnode.props[KEY_TYPE])
-        if cnode.props[KEY_LIST]:
-            value = args.get(target, []) + [value]
-
-        args[target] = value
-        return cnode
+        return self.__capture_arg(node.var_child, token, args)
 
     def call(self, tokens):
         self.cmdline = tokens
@@ -183,28 +206,29 @@ main feature:
 
 details:
 * special format in tokens, which support the following behaviors
-    1. pass token (string) as args with some name
+    v 1. pass token (string) as args with some name
         * e.g.
             ["set", "feature", "@enable"], handle_set_feature(enable)
             > set feature true
             pass string "true" as argument "enable" in handle_set_feature
-    2. allow single token that accept dynamic number of args in command
+    [v] 2. allow single token that accept dynamic number of args in command
         * e.g.
             ["show", "slot", "@idlist..."], handle_show_slot(idlist)
             > show slot 1 2 3
             pass list of string ["1", "2", "3"] as argument "idlist" in handle_show_slot
-    3. type check/convert for arguments
+    [v] 3. type check/convert for arguments
         * e.g.
             ["show", "slot", "@idlist(int)..."], handle_show_slot(idlist)
             > show slot 1 2 3
             pass list of int [1, 2, 3] as argument "idlist" in handle_show_slot
-    4. match prefix, match abbreviation
+    [v] 4.1 match prefix
+    4.2 match abbreviation
         * e.g.
             ["set", "fs|filesystem", "@fstype"], handle_set_filesystem(fstype)
             all the following commands matchs, if no other possible token start with files*
-            > set fs ext4
-            > set files ext4
-    5. allow some commands to be hidden, which won't be shown in help
+            > set fs ext4 # abbreviation
+            > set files ext4 # prefix
+    [v] 5. allow some commands to be hidden, which won't be shown in help
 
 * show helpful messages when command line matchs no rules
 * show candidates of commands when passing special token (e.g. ? like cisco console)
